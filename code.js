0ec9dd14cd816705320c5754773b40361eabffd1
lilpunnn <52867100+lilpunnn@users.noreply.github.com>
2021-05-02 13:20:56 +0000

    Update code.js

--- code.js
+++ code.js
@@ -2,28 +2,30 @@ const addToDoButton = document.getElementById("addToDoButton");
 const inputField = document.getElementById("inputField");
 const todoContainer = document.getElementById("todoContainer");
 const paragraph = document.createElement("P");
-//check Enter press
-addEventListener("keypress", (e) => {
-     if (e.key === "Enter") {
-          createTask();
-     }
-});
-
 // check click
 addToDoButton.addEventListener("click", function () {
-     createTask();
-     paragraph.addEventListener("click", function () {
-          paragraph.style.textDecoration = "line-through";
-     });
-     paragraph.addEventListener("dblclick", function () {
-          todoContainer.removeChild(paragraph);
-     });
+	createTask();
+});
+
+//check Enter press
+addEventListener("keypress", (e) => {
+	if (e.key === "Enter") {
+		createTask();
+	}
 });
 
 // create Task
 function createTask() {
-     paragraph.classList.add("paragraph-styling");
-     paragraph.innerText = inputField.value;
-     todoContainer.appendChild(paragraph);
-     inputField.value = "";
+	paragraph.classList.add("paragraph-styling");
+	paragraph.innerText = inputField.value;
+	todoContainer.appendChild(paragraph);
+	inputField.value = "";
 }
+//Task completed
+
+paragraph.addEventListener("click", function () {
+	paragraph.style.textDecoration = "line-through";
+});
+paragraph.addEventListener("dblclick", function () {
+	todoContainer.removeChild(paragraph);
+});
