aa64c7317549ca5055ea01d06076a0fdfc8c1dab
lilpunnn <52867100+lilpunnn@users.noreply.github.com>
2021-05-02 13:12:36 +0000

    Update code.js

--- code.js
+++ code.js
@@ -1,6 +1,7 @@
-let addToDoButton = document.getElementById("addToDoButton");
-let inputField = document.getElementById("inputField");
-let todoContainer = document.getElementById("todoContainer");
+const addToDoButton = document.getElementById("addToDoButton");
+const inputField = document.getElementById("inputField");
+const todoContainer = document.getElementById("todoContainer");
+const paragraph = document.createElement("P");
 //check Enter press
 addEventListener("keypress", (e) => {
      if (e.key === "Enter") {
@@ -21,7 +22,6 @@ addToDoButton.addEventListener("click", function () {
 
 // create Task
 function createTask() {
-     var paragraph = document.createElement("P");
      paragraph.classList.add("paragraph-styling");
      paragraph.innerText = inputField.value;
      todoContainer.appendChild(paragraph);
